058c8d33106729f66c4f91afa34b4e2228de6746
mananiko005 <166157246+mananiko005@users.noreply.github.com>
2024-09-18 20:06:04 +0400

    exercise

---
+++ script.js
@@ -0,0 +1,38 @@
+let Num1=100;
+
+if(Num1<50){
+    console.log(`ნაკლებია (50)-ზე`);
+}
+else if (Num1>20){
+    console.log(`მეტია(20)-ზე`);
+}
+else{
+    console.log(`შეცდომა`);
+}
+
+
+let User=`მარიამ`;
+
+if(User=`მარიამ`){
+    console.log(true);}
+else{
+console.log(false);
+}
+
+
+let User1=`მარიამ`;
+switch(User1){
+    case `მარიამ`:
+        console.log(true);
+        break;
+        default: 
+        console.log(false);
+}
+
+let User2=`მარიამ`;
+switch(User2){
+    case `მარიამ`:
+        alert(true);
+        break;
+        default: (false);
+}
